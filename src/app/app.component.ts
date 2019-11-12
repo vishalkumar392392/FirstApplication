c2119ed4ec4eb61aebb3e8894ea97468b2f0c5fd
vishal <vishalgate2019@gmail.com>
2019-11-13 02:58:02 +0530

    night

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgModule } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = "myFirstApp";
+  details = {
+    candidate: "",
+    branch: ""
+  };
+  storge = [];
+  onSubmit(theForm: NgForm) {
+    // (this.details.candidate = theForm.value.candidate),
+    //   (this.details.branch = theForm.value.branch);
+    let {
+      value: { branch, candidate }
+    } = theForm;
+    this.storge.push({
+      candidate,
+      branch
+    });
+    console.log(this.storge);
+  }
 }
